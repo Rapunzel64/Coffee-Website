2435c054d015347c3022b388504515683ffdf1c5
Anjali Chauhan <anjalichauhan8768123@gmail.com>
2025-07-31 17:33:30 +0530

    finalise my website

--- script.js
+++ script.js
@@ -1,47 +1,42 @@
+const navbarLinks = document.querySelectorAll(".nav-menu .nav-link");
 const menuOpenButton = document.querySelector("#menu-open-button");
-const navLink = document.querySelector(".nav-menu .nav-link");
 const menuCloseButton = document.querySelector("#menu-close-button");
 menuOpenButton.addEventListener("click", () => {
-    /*toggle mobile menu visiblity*/
-    document.body.classList.toggle("show-mobile-menu");
+  // Toggle mobile menu visibility
+  document.body.classList.toggle("show-mobile-menu");
 });
-
-/*close menu when the close button is clicked */
+// Close menu when the close button is clicked
 menuCloseButton.addEventListener("click", () => menuOpenButton.click());
-
-/*close menu when the nav link is clicked */
-navLink.forEach(link => {
-  link.addEventListener("click", () =>menuOpenButton.click());
+// Close menu when nav link is clicked
+navbarLinks.forEach((link) => {
+  link.addEventListener("click", () => menuOpenButton.click());
 });
-
-// Initialize Swiper
-const swiper = new Swiper('.slider-wrapper', {
+/* Initializing Swiper */
+let swiper = new Swiper(".slider-wrapper", {
   loop: true,
   grabCursor: true,
   spaceBetween: 25,
-
-  // If we need pagination
+  // Pagination bullets
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
     clickable: true,
     dynamicBullets: true,
   },
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
-
-  // Responsive breakpoints
+  /* Responsive breakpoints */
   breakpoints: {
-    324: {
-        slidesPerView: 1
+    0: {
+      slidesPerView: 1,
     },
     768: {
-        slidesPerView: 2
+      slidesPerView: 2,
     },
-    1000: {
-        slidesPerView: 3
-    }
-  }
+    1024: {
+      slidesPerView: 3,
+    },
+  },
 });
